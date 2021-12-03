0a7d41bb969f9ad6bd210911ac450c591d5d25e5
Thejus Paul <thejuspaul@pm.me>
2021-12-03 16:58:11 +0530

    Added routes for delete method

--- routes.ts
+++ routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import { getDramas, getDrama, addDrama, updateDrama } from "./controllers/dramas.ts";
+import { getDramas, getDrama, addDrama, updateDrama, deleteDrama } from "./controllers/dramas.ts";
 
 const router = new Router();
 
 router.get('/api/v1/dramas', getDramas)
     .get('/api/v1/dramas/:id', getDrama)
     .post('/api/v1/dramas', addDrama)
-    .put('/api/v1/dramas/:id', updateDrama);
+    .put('/api/v1/dramas/:id', updateDrama)
+    .delete('/api/v1/dramas/:id', deleteDrama);
 
 export default router;
