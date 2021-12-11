6d658b2ea2e8372297ffd981b1b988d4b96c4019
Thejus Paul <thejuspaul@pm.me>
2021-12-11 22:45:57 +0530

    Added centralized deps.ts and lock file

--- routes.ts
+++ routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
+import { Router } from "./deps.ts";
 import { getDramas, getDrama, addDrama, updateDrama, deleteDrama } from "./controllers/dramas.ts";
 
 const router = new Router();
