39ce65ca8866884bacdb9e1eece362eea1819225
Thejus Paul <thejuspaul@pm.me>
2021-12-13 23:34:51 +0530

    Refactored routes to prefix api/v1

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "../deps.ts";
 import { getDramas, getDrama, addDrama, updateDrama, deleteDrama } from "./controllers/dramas.ts";
 
-const router = new Router();
+const router = new Router({ prefix: "/api/v1" });
 
-router.get('/api/v1/dramas', getDramas)
-    .get('/api/v1/dramas/:id', getDrama)
-    .post('/api/v1/dramas', addDrama)
-    .put('/api/v1/dramas/:id', updateDrama)
-    .delete('/api/v1/dramas/:id', deleteDrama);
+router.get('/dramas', getDramas)
+    .get('/dramas/:id', getDrama)
+    .post('/dramas', addDrama)
+    .put('/dramas/:id', updateDrama)
+    .delete('/dramas/:id', deleteDrama);
 
 export default router;
