d920c127514f239ab25d678efab13b62959f47de
Thejus Paul <32426070+Thejus-Paul@users.noreply.github.com>
2021-12-13 19:28:56 -1200

    Added update drama with given ID functionality (#22)
    * Renamed Dramas collection to dramas
    
    * Removed unwanted Drama type
    
    * Refactored updateDrama method to update drama on DB

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,16 @@
 import { Bson } from "../deps.ts";
 
 export interface DramaSchema {
-    _id: Bson.ObjectId;
-    name: string;
-    otherNames: string;
-    country: string;
-    genre: string;
-    aired: string;
-    status: string;
-    summary: string;
-    image: string;
-    latestEpisode: number;
-    slug: string;
-    hash: string;
-  }
-
-  export interface Drama {
-    id: string;
-    name: string;
-    latestEpisode: number;
-  }
+  _id: Bson.ObjectId;
+  name: string;
+  otherNames: string;
+  country: string;
+  genre: string;
+  aired: string;
+  status: string;
+  summary: string;
+  image: string;
+  latestEpisode: number;
+  slug: string;
+  hash: string;
+}
