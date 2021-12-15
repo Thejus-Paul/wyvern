03421df8f39e7202ada3fe72ca5fd5be687d083c
Thejus Paul <32426070+Thejus-Paul@users.noreply.github.com>
2021-12-15 06:25:37 -1200

    Added tests for environment variables (#24)
    * Added assertion dependency package
    
    * Added tests for environment variables

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 export { Application, Router, Request, Response } from 'https://deno.land/x/oak/mod.ts';
 export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.28.1/mod.ts";
 export { config } from 'https://deno.land/x/dotenv/mod.ts';
+export { assertExists, assertNotEquals, assertMatch, assertNotMatch, AssertionError } from "https://deno.land/std@0.117.0/testing/asserts.ts";
