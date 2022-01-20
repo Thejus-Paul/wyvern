97136043c2b7bbeccbace2868e8d26990b342793
Thejus Paul <thejuspaul@pm.me>
2022-01-20 21:13:57 +0530

    Added logging scripts

--- config.ts
+++ config.ts
@@ -1,15 +1,8 @@
-const { env } = Deno;
-
-export const APP_HOST = env.get("APP_HOST");
-
-export const DB_USERNAME = env.get("DB_USERNAME");
-
-export const DB_PASSWORD = env.get("DB_PASSWORD");
-
-export const DB_CLUSTER_URL = env.get("DB_CLUSTER_URL");
-
-export const DB_NAME = env.get("DB_NAME");
-
-export const APP_PORT = env.get("APP_PORT");
-
-console.log(env.toObject());
+export const {
+  APP_HOST,
+  APP_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_CLUSTER_URL,
+  DB_NAME,
+} = Deno.env.toObject();
